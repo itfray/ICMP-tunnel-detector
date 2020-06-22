be9520359e27312061eba10b7eaf642417b6c273
Efarov Ilya <efarov.ily@mail.ru>
2020-06-23 01:49:30 +0900

    add protocol number constants

--- net_header.py
+++ net_header.py
@@ -1,6 +1,14 @@
 import abc
 
 
+# Protocol numbers
+PROTO_ICMP = 1
+PROTO_IPv4 = 4
+PROTO_IPv6 = 41
+PROTO_TCP = 6
+PROTO_UDP = 17
+
+
 # interface for any class network header
 class InterfaceNetHeader(abc.ABC):
     @abc.abstractmethod
